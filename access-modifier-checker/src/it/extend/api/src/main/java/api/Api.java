331a2af9a4e8aba8de92f929617fa464da225909
Daniel Beck <daniel-beck@users.noreply.github.com>
2020-03-05 23:57:41 +0100

    Add failing tests around subclasses

---
+++ access-modifier-checker/src/it/extend/api/src/main/java/api/Api.java
@@ -0,0 +1,11 @@
+package api;
+
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
+public class Api {
+    @Restricted(NoExternalUse.class)
+    public void notReallyPublic() {
+
+    }
+}
